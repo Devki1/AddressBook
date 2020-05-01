1a41c1d79c3bafd784da291117033b469a33114f
Devki1 <gdev3195@gmail.com>
2020-05-01 20:02:17 +0530

    ADDED:Method saveAs file method and implementation

--- src/main/java/com/bridgelabz/addressbook/services/IAddressBookService.java
+++ src/main/java/com/bridgelabz/addressbook/services/IAddressBookService.java
@@ -3,7 +3,6 @@ package com.bridgelabz.addressbook.services;
 import com.bridgelabz.addressbook.model.AddressDetails;
 import com.bridgelabz.addressbook.model.PersonDetails;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,8 +16,14 @@ public interface IAddressBookService {
     void sortByPersonName(String filePath);
 
     List printEntries(String filePath);
+
     Boolean createAddressBook(String fileName);
+
     Boolean openExistingAddressBook(String fileName);
-    String deleteFile( String fileName);
+
+    String deleteFile(String fileName);
+
     public void saveAddressBook(String path, ArrayList<PersonDetails> data);
+
+    public boolean saveAsAddressBook(String filePath, PersonDetails personDetails);
 }
